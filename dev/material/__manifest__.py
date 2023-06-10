0ba0c415ad2913e9eb77c8df732542f7227f2cb1
hankusa1709 <handyk1709@gmail.com>
2023-06-10 15:13:55 +0700

    created init and manifest module

---
+++ dev/material/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Material",
+    "author": "Handy Kusuma",
+    "depends": ["mail"],
+    "data": [
+        'data/sequence.xml',
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/material.xml',
+        'views/supplier.xml'
+    ]
+}
